daf29d55a76d799d6962d66ca40631f3f57b8588
spoofhopper <morgan.holland@gmail.com>
2015-09-27 13:51:10 -0700

    create message _form, new, edit, and index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
+//= require bootstrap-datepicker-rails
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
