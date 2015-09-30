b3a7adc156fc7595463ccf7af0b797cec43d76e2
Morgan Holland <morgan.holland@gmail.com>
2015-09-30 15:52:12 -0700

    cleaned up all pages to match theme

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,7 +16,7 @@ class GroupsController < ApplicationController
     def create
       @group = Group.new(group_params)
       if @group.save
-        redirect_to @group
+        redirect_to group_messages_path(@group)
       else
         render 'new'
       end
