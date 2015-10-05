837a908852be2c8591c31cb7000c4c74ccb2e794
Morgan Holland <morgan.holland@gmail.com>
2015-10-05 14:38:30 -0700

    got rufus working, thanks D

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,6 +12,7 @@
 #
 
 require 'rufus-scheduler'
+require 'twilio-ruby'
 
 class Group < ActiveRecord::Base
   belongs_to :user
@@ -19,39 +20,8 @@ class Group < ActiveRecord::Base
   has_many :contacts
   validates :name, presence: true,
                     length: { minimum: 5, maximum: 50 }
+
+
+
+
 end
-
-
-
-
-def prepare_group_message_for_sending
-  group = {morgan: 14157938267}
-  group.each do |k,v|
-    message = "This is a test"
-    compose_message(v, message)
-  end
-end
-
-
-
-def send_message_with_twilio(student_mobile, message_body)
-  client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_TOKEN)
-  client.account.messages.create(from: TWILIO_NUMBER, to: student_mobile, body: message_body)
-  puts "Sent Message to #{student_mobile}!"
-end
-
-
-
-def send_scheduled_messages
-  scheduler = Rufus::Scheduler.new
-  test = 3
-  while test <= 2
-    scheduler.every '5s' do
-      send_message_to_group
-      test += 1
-    end
-    scheduler.join
-  end
-end
-
-send_scheduled_messages
--- app/models/message.rb
+++ app/models/message.rb
@@ -17,13 +17,46 @@
 #  MediaURL     :string
 #
 
-require 'twilio-ruby'
+
 
 class Message < ActiveRecord::Base
   belongs_to :group
   has_many :users, through: :groups
   validates :body, presence: true,
                     length: { minimum: 5, maximum: 160 }
+  validates :group_id, presence: true
+  validates :date, presence: true
+  after_create :schedule_message
+
+
+
+ def self.send_message_with_twilio ( phone_number, name, body )
+
+   sid = "AC74db4ed4a2b6e400880205014d384761"
+   token = "e6ded52b2286ca47517ae3641be3f54d"
+
+    client = Twilio::REST::Client.new(sid, token)
+
+    client.account.messages.create(
+    from: "+14153608229",
+    to: phone_number,
+    body: body
+    #media_url: "image url"
+    )
+
+    puts "Sent Message to #{name}!"
+
+  end
+
+  def schedule_message
+    scheduler = Rufus::Scheduler.new
+    scheduler.at date do
+      group.contacts.each do | contact |
+        Message.send_message_with_twilio(contact.phone_number, contact.name, body)
+      end
+    end
+    scheduler.join
+  end
 
 
 end
