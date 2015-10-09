35471635184b37e3ced3e92ccdff002f420b6fb4
spoofhopper <morgan.holland@gmail.com>
2015-10-09 12:12:24 -0700

    switched to test twilio keys

--- app/models/message.rb
+++ app/models/message.rb
@@ -32,36 +32,23 @@ class Message < ActiveRecord::Base
 
  def self.send_message_with_twilio ( phone_number, name, body )
 
-   #sid = "AC74db4ed4a2b6e400880205014d384761"
-   #token = "e6ded52b2286ca47517ae3641be3f54d"
-
-    # client = Twilio::REST::Client.new(sid, token)
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 
-    # client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
-    # client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
     client.account.messages.create(
-    from: Rails.application.secrets.twilio_phone_number,
-    # from: "+14153608229",
-    # from: TWILIO_CONFIG['from'],
-    # from: ENV["TWILIO_PHONE_NUMBER"],
-    to: phone_number,
-    body: body
-    #media_url: "image url"
+      from: Rails.application.secrets.twilio_phone_number,
+      to: phone_number,
+      body: body
+      #media_url: "image url"
     )
 
     puts "Sent Message to #{name}!"
 
   end
 
-
-def schedule_message
+  def schedule_message
       group.contacts.each do | contact |
         Message.delay(run_at: date).send_message_with_twilio(contact.phone_number, contact.name, body)
       end
-end
-
-
-
+  end
 
 end
--- config/initializers/twilio.rb
+++
@@ -1,3 +0,0 @@
-# path = File.join(Rails.root, "config/twilio.yml")
-# TWILIO_CONFIG = YAML.load(File.read(path))[Rails.env] || {'sid' => '', 'from' => '', 'token' => ''}
-# TWILIO_CONFIG = Rails.application.config_for(:twilio)
