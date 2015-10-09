7b0e7101f2da878afe408142c56499fd69e97104
spoofhopper <morgan.holland@gmail.com>
2015-10-09 12:17:23 -0700

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true #changed from false to get images to work on Heroku
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
